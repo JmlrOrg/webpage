424f07390842ed790d31960aecbd0306ce77dedb
Fabian Pedregosa <f@bianp.net>
2018-08-14 14:54:15 -0700

    upload script

--- bin/upload.py
+++ bin/upload.py
@@ -10,7 +10,13 @@ path = os.path.join(path, 'v2')
 command = 'rsync -arvz %s %s@%s' % (origin, user, path)
 print(command)
 
+ssh_newkey = 'Are you sure you want to continue connecting'
 child = pexpect.spawn(command)
-child.expect('Password:')
-child.sendline(passwd)
+i = p.expect([ssh_newkey,'password:',pexpect.EOF])
+if i==0:
+    print('I say yes')
+    p.sendline('yes')
+    i = p.expect([ssh_newkey,'password:',pexpect.EOF])
+if i==1:
+    child.sendline(passwd)
 child.interact()

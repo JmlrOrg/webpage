1e6261ec4c2e428c6780312371b53d0f429ce946
Alp Kucukelbir <akucukelbir@gmail.com>
2019-06-10 09:01:43 -0400

    accent changes

--- src/utils.py
+++ src/utils.py
@@ -3,22 +3,22 @@ from bibtexparser.bparser import BibTexParser
 from bibtexparser.customization import convert_to_unicode
 
 accents = [
-   ["'a", "á"], ['"a', "ä"], ["^a", "â"], ["`a", "à"],
+   ["'a", "á"], ['"a', "ä"], ["^a", "â"], ["`a", "à"], ["~a", "ã"],
    ["'e", "é"], ["'E", "É"], ['"e', "ë"], ["^e", "ê"], ["`e", "è"],
    ["'i", "í"], ['"i', "ï"], ["^i", "î"], ["`i", "ì"],
    ["'o", "ó"], ['"o', "ö"], ["^o", "ô"], ["`o", "ò"],
    ["'u", "ú"], ['"u', "ü"], ["^u", "û"], ["`u", "ù"],
    ['"U', "Ü"], ['"u', "ü"],
    ['O', "Ø"],
-   ['o', "ø"], ['l', 'ł'], ["'n", "ń"], 
+   ['o', "ø"], ['l', 'ł'], ["'n", "ń"],
    ['L', 'Ł'], ['&', '&amp;'], ["'c", "ć"],
-   ['~n', 'ñ']
+   ['~n', 'ñ'], ["'n", "ń"]
 ]
 
 # this are combinations of the form {\a{b}}
 # where a is the first character and b the second one
 accents2 = [
-    ['vS', 'Š'], ['vs', 'š'], ['vc', 'č'], ["cC", "Ç"], ['ua', 'ă'],  ['Ho', "ő"], ["vZ", "Ž"],["cc", "ç"], ['ug', 'ğ'],
+    ['vS', 'Š'], ['vs', 'š'], ['vc', 'č'], ["cC", "Ç"], ['ua', 'ă'],  ['Ho', "ő"], ["vZ", "Ž"],["cc", "ç"], ['ug', 'ğ'], ['.z', 'ż']
 ]
 
 def xml_string(text):
@@ -30,7 +30,7 @@ def xml_string(text):
         text = text.replace('{{\\%s}}' % tex, utf8)          # {{\"a}}
         # text = text.replace('\\%s' % tex, utf8)            # \"a
     for tex, utf8 in accents2:
-        text = text.replace('{\\%s{%s}}' % tuple(tex), utf8) 
+        text = text.replace('{\\%s{%s}}' % tuple(tex), utf8)
     return text
 
 

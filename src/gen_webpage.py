72e14f0c87dfe6aff6d1e31d7f4e6c6fbf159b3b
Fabian Pedregosa <pedregosa@google.com>
2020-03-29 18:03:08 -0400

    V13 and many tweaks

--- src/gen_webpage.py
+++ src/gen_webpage.py
@@ -14,91 +14,72 @@ if not os.path.exists("output"):
     os.mkdir("output")
 
 
-def render_webpage(prefix, page):
-    if not os.path.exists(os.path.join("output", prefix)):
-        os.mkdir(os.path.join("output", prefix))
+def render_webpage(env, prefix, page, base_url):
     with open(os.path.join("output", prefix, page), "w") as f:
-        t_path = os.path.join(prefix, "%s" % page)
-        template = env.get_template(t_path)
-        out = template.render(**info, year=YEAR)
-        f.write(out)
-
-
-# .. beta webpage ..
-prefix = "beta"
-if not os.path.exists(os.path.join("output", prefix)):
-    os.mkdir(os.path.join("output", prefix))
-env = Environment(
-    loader=FileSystemLoader(os.path.join("templates", "beta")),
-    autoescape=select_autoescape(["html", "xml"]),
-)
-
-for page in [
-    "index.html",
-    "editorial-board.html",
-    "news.html",
-    "author-info.html",
-    "contact.html",
-    "editorial-board-reviewers.html",
-    "stats.html",
-]:
-    base_url = "/" + prefix
-    with open(os.path.join("output", prefix, page), "w") as f:
-        template = env.get_template("%s" % page)
+        template = env.get_template(page)
         out = template.render(
             **info,
             year=YEAR,
             base_url=base_url,
             home_active=(page == "index.html"),
             editorial_board_active=(page == "editorial-board.html"),
-            stats_active=(page == 'stats.html')
+            stats_active=(page == "stats.html")
         )
         f.write(out)
-# .. end beta webpage ..
+
+
+# .. beta webpage ..
+
 
 # .. current webpage ..
-prefix = ""
-base_url = prefix
-env = Environment(
-    loader=FileSystemLoader(os.path.join("templates", prefix)),
-    autoescape=select_autoescape(["html", "xml"]),
-)
+for prefix in ["", "beta"]:
+    if not os.path.exists(os.path.join("output", prefix)):
+        os.mkdir(os.path.join("output", prefix))
 
-
-for page in [
-    "author-info.html",
-    "contact.html",
-    "editorial-board.html",
-    "editorial-board-reviewers.html",
-    "news.html",
-    "index.html",
-    "reviewer-guide.html",
-    "stats.html",
-]:
-    render_webpage(prefix, page)
-
-# MLOSS webpage
-if not os.path.exists("output/mloss/"):
-    os.mkdir("output/mloss/")
-
-render_webpage("", "mloss/mloss-info.html")
-with open(os.path.join("output", prefix, "mloss/index.html"), "w") as f:
-    vol = 13
-    list_info_mloss = []
-    while True:
-        # get all info for v18 and onwards
-        try:
-            info_list = utils.get_info(vol)
-            info_mloss = filter(
-                lambda x: x.get("special_issue", "") == "MLOSS", info_list
-            )
-            list_info_mloss.append(info_mloss)
-            vol += 1
-        except FileNotFoundError:
-            break
-
-    editorial_board_template = env.get_template("mloss/index.html")
-    out = editorial_board_template.render(
-        list_info_mloss=list_info_mloss, base_url=base_url
+    if prefix == "":
+        base_url = ""
+    else:
+        base_url = "/" + prefix
+    env = Environment(
+        loader=FileSystemLoader(os.path.join("templates", prefix)),
+        autoescape=select_autoescape(["html", "xml"]),
     )
-    f.write(out)
+
+    for page in [
+        "author-info.html",
+        "contact.html",
+        "editorial-board.html",
+        "editorial-board-reviewers.html",
+        "news.html",
+        "index.html",
+        "reviewer-guide.html",
+        "stats.html",
+    ]:
+        render_webpage(env, prefix, page, base_url)
+
+    # MLOSS webpage
+    mloss_dir = os.path.join("output", prefix, "mloss")
+    if not os.path.exists(mloss_dir):
+        os.mkdir(mloss_dir)
+
+    render_webpage(env, prefix, "mloss/mloss-info.html", base_url)
+    with open(os.path.join("output", prefix, "mloss/index.html"), "w") as f:
+        start_vol = 13
+        list_info_mloss = []
+        while True:
+            # get all info for v18 and onwards
+            try:
+                info_list = utils.get_info(start_vol)
+                info_mloss = filter(
+                    lambda x: x.get("special_issue", "") == "MLOSS", info_list
+                )
+                list_info_mloss.append(info_mloss)
+                start_vol += 1
+            except FileNotFoundError:
+                break
+
+        editorial_board_template = env.get_template("mloss/index.html")
+        out = editorial_board_template.render(
+            list_info_mloss=list_info_mloss, base_url=base_url
+        )
+        f.write(out)
